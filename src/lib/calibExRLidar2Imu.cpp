ffc8850f2b8f0858156c1ba8824a532cdb4c2c3c
chennuo0125-HIT <37269147+chennuo0125-HIT@users.noreply.github.com>
2020-12-21 11:06:23 +0800

    remove optimize fail check

--- src/lib/calibExRLidar2Imu.cpp
+++ src/lib/calibExRLidar2Imu.cpp
@@ -309,12 +309,12 @@ void CalibExRLidarImu::optimize()
     result.normalize();
 
     // check whether optimize fail
-    double angle = fabs(q_l_b_.angularDistance(result));
-    if (angle > 0.5236)
-    {
-        cout << "the difference between before and after optimze is " << angle << " which greater than given threshold 0.5236 !!!" << endl;
-        return;
-    }
+    // double angle = fabs(q_l_b_.angularDistance(result));
+    // if (angle > 0.5236)
+    // {
+    //    cout << "the difference between before and after optimze is " << angle << " which greater than given threshold 0.5236 !!!" << endl;
+    //    return;
+    // }
     q_l_b_ = result;
 }
 
